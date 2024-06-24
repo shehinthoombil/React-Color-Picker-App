e4cb43eb9031332f5cff6764a79b7c2505973673
Shehin Thoombil <shehinthoombil@gmail.com>
2024-06-24 21:05:04 +0530

    picker work successfully

--- src/ColorPicker.jsx
+++ src/ColorPicker.jsx
@@ -1,9 +1,24 @@
 import React,{useState} from 'react'
 
 function ColorPicker() {
+
+    const [color, setColor] = useState("#ffffff")
+
+    const handleColorChange = (event)=>{
+        setColor(event.target.value)
+    }
+
+
   return (
-   <>
-   </>
+   <div className='color-picker-container'>
+    <h1>Color Picker</h1>
+    <div className='color-display' style={{backgroundColor: color}}>
+    <p>Selected Color: {color}</p>
+    </div>
+
+    <label>Select a Color :</label>
+    <input type="color" value={color} onChange={handleColorChange}/>
+   </div>
   )
 }
 
